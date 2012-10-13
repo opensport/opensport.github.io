62b1f13b0ec7e74481dbd5597bc9e645e6802181
Gerald Bauer <gerald.bauer@gmail.com>
2012-10-13 20:50:05 +0200

    polish world quali rounds

--- README.md
+++ README.md
@@ -7,25 +7,29 @@ A Free Open Sports Database & Schema
 
 Get a copy of the `sport.db` file. That's it.
 
-### Events include
+## Events
 
-National football teams (e.g. Austria, England, etc.)
+### Football (Soccer)
+
+International w/ national teams (e.g. Austria, England, etc.)
 
 * [World Cup Quali 2012/13](https://github.com/geraldb/sport.db/tree/master/db/world)
 * [UEFA European Football Championship (Euro) 2012](https://github.com/geraldb/sport.db/tree/master/db/euro)
 
-Football club teams (e.g. FC Barcelona, Manchester Utd, etc.) / International
+International w/ football club teams (e.g. FC Barcelona, Manchester Utd, etc.)
 
 * [UEFA Champions League 2011/12, 2012/13](https://github.com/geraldb/sport.db/tree/master/db/cl)
 * [UEFA Europa League 2011/12](https://github.com/geraldb/sport.db/tree/master/db/cl)
 
-Football club teams / National
+National leagues & cups w/ football club teams 
 
 * [English Permier League 2012/13](https://github.com/geraldb/sport.db/tree/master/db/en)
 * [Deutsche Bundesliga 2012/13](https://github.com/geraldb/sport.db/tree/master/db/de)
 * [Österreichische Bundesliga 2011/12, 2012/13](https://github.com/geraldb/sport.db/tree/master/db/at), [ÖFB Cup 2011/12, 2012/13](https://github.com/geraldb/sport.db/tree/master/db/at)
 * [Romania Liga 1 2012/13](https://github.com/geraldb/sport.db/tree/master/db/ro)
 * [México Primera División Apertura 2012](https://github.com/geraldb/sport.db/tree/master/db/mx)
+
+### Hockey
 
 Hockey club teams
 
--- db/world/quali_2012_13_c.rb
+++ db/world/quali_2012_13_c.rb
@@ -5,14 +5,22 @@
 ###########################################
 ##  WM 2014 - Qualification Europe Group C
 
-world = Event.find_by_key!( 'wmql' )
+world = Event.find_by_key!( 'wmq' )
 
-world1 = Round.find_by_event_id_and_pos!( world.id, 1 ) # 7.+11. Sep 2012
-world2 = Round.find_by_event_id_and_pos!( world.id, 2 ) # 12.+16. Okt 2012
-world3 = Round.find_by_event_id_and_pos!( world.id, 3 ) # 22.+26. Mär 2013
-world4 = Round.find_by_event_id_and_pos!( world.id, 4 ) #  7.+11. Jun 2013
-world5 = Round.find_by_event_id_and_pos!( world.id, 5 ) #  6.+10. Sep 2013
-world6 = Round.find_by_event_id_and_pos!( world.id, 6 ) # 11.+15. Okt 2013
+world1  = Round.find_by_event_id_and_pos!( world.id, 1 ) # 7. Sep 2012
+world2  = Round.find_by_event_id_and_pos!( world.id, 2 ) # 11. """"""
+world3  = Round.find_by_event_id_and_pos!( world.id, 3 ) # 12. Okt 2012
+world4  = Round.find_by_event_id_and_pos!( world.id, 4 ) # 16. """""""
+world5  = Round.find_by_event_id_and_pos!( world.id, 5 ) # 22. Mär 2013
+world6  = Round.find_by_event_id_and_pos!( world.id, 6 ) # 26. """"""
+world7  = Round.find_by_event_id_and_pos!( world.id, 7 ) #  7. Jun 2013
+world8  = Round.find_by_event_id_and_pos!( world.id, 8 ) # 11. """""""
+world9  = Round.find_by_event_id_and_pos!( world.id, 9 ) #  6. Sep 2013
+world10 = Round.find_by_event_id_and_pos!( world.id, 10 ) # 10. """""""
+world11 = Round.find_by_event_id_and_pos!( world.id, 11 ) # 11. Okt 2013
+world12 = Round.find_by_event_id_and_pos!( world.id, 12 ) # 15. """"""""
+
+
 
 
 #################################3
@@ -49,42 +57,54 @@ world.teams << swe
 
 games_world1c = [
  [ 1,  ger, [3,0], fro, Time.cet( '2012-09-07 00:00' ) ],
- [ 2,  kaz, [1,2], irl, Time.cet( '2012-09-07 00:00' ) ],
+ [ 2,  kaz, [1,2], irl, Time.cet( '2012-09-07 00:00' ) ]]
+
+games_world2c = [
  [ 3,  aut, [1,2], ger, Time.cet( '2012-09-11 00:00' ) ],
  [ 4,  swe, [2,0], kaz, Time.cet( '2012-09-11 00:00' ) ]]
 
-games_world2c = [
- [  5,  irl, [], ger, Time.cet( '2012-10-12 00:00' ) ],
- [  6,  fro, [], swe, Time.cet( '2012-10-12 00:00' ) ],
- [  7,  kaz, [], aut, Time.cet( '2012-10-12 00:00' ) ],
- [  8,  ger, [], swe, Time.cet( '2012-10-16 00:00' ) ],
- [  9,  fro, [], irl, Time.cet( '2012-10-16 00:00' ) ],
- [ 10,  aut, [], kaz, Time.cet( '2012-10-16 00:00' ) ]]
-
 games_world3c = [
+ [ 5,  irl, [1,6], ger, Time.cet( '2012-10-12 20:45' ) ],
+ [ 6,  fro, [1,2], swe, Time.cet( '2012-10-12 18:00' ) ],
+ [ 7,  kaz, [0,0], aut, Time.cet( '2012-10-12 18:00' ) ]]
+
+games_world4c = [
+ [  8,  ger, [], swe, Time.cet( '2012-10-16 20:45' ) ],
+ [  9,  fro, [], irl, Time.cet( '2012-10-16 20:00' ) ],
+ [ 10,  aut, [], kaz, Time.cet( '2012-10-16 20:30' ) ]]
+
+games_world5c = [
  [ 11,  kaz, [], ger, Time.cet( '2013-03-22 00:00' ) ],
  [ 12,  aut, [], fro, Time.cet( '2013-03-22 00:00' ) ],
- [ 13,  swe, [], irl, Time.cet( '2013-03-22 00:00' ) ],
+ [ 13,  swe, [], irl, Time.cet( '2013-03-22 00:00' ) ]]
+
+games_world6c = [
  [ 14,  ger, [], kaz, Time.cet( '2013-03-26 00:00' ) ],
  [ 15,  irl, [], aut, Time.cet( '2013-03-26 00:00' ) ]]
 
-games_world4c = [
+games_world7c = [
  [ 16,  irl, [], fro, Time.cet( '2013-06-07 00:00' ) ],
- [ 17,  aut, [], swe, Time.cet( '2013-06-07 00:00' ) ],
+ [ 17,  aut, [], swe, Time.cet( '2013-06-07 00:00' ) ]]
+
+games_world8c = [
  [ 18,  swe, [], fro, Time.cet( '2013-06-11 00:00' ) ]]
 
-games_world5c = [
+games_world9c = [
  [ 19,  ger, [], aut, Time.cet( '2013-09-06 00:00' ) ],
  [ 20,  kaz, [], fro, Time.cet( '2013-09-06 00:00' ) ],
- [ 21,  irl, [], swe, Time.cet( '2013-09-06 00:00' ) ],
+ [ 21,  irl, [], swe, Time.cet( '2013-09-06 00:00' ) ]]
+
+games_world10c = [
  [ 22,  fro, [], ger, Time.cet( '2013-09-10 00:00' ) ],
  [ 23,  aut, [], irl, Time.cet( '2013-09-10 00:00' ) ],
  [ 24,  kaz, [], swe, Time.cet( '2013-09-10 00:00' ) ]]
 
-games_world6c = [
+games_world11c = [
  [ 25,  ger, [], irl, Time.cet( '2013-10-11 00:00' ) ],
  [ 26,  fro, [], kaz, Time.cet( '2013-10-11 00:00' ) ],
- [ 27,  swe, [], aut, Time.cet( '2013-10-11 00:00' ) ],
+ [ 27,  swe, [], aut, Time.cet( '2013-10-11 00:00' ) ]]
+
+games_world12c = [
  [ 28,  swe, [], ger, Time.cet( '2013-10-15 00:00' ) ],
  [ 29,  fro, [], aut, Time.cet( '2013-10-15 00:00' ) ],
  [ 30,  irl, [], kaz, Time.cet( '2013-10-15 00:00' ) ]]
@@ -96,6 +116,12 @@ Game.create_from_ary!( games_world3c, world3 )
 Game.create_from_ary!( games_world4c, world4 )
 Game.create_from_ary!( games_world5c, world5 )
 Game.create_from_ary!( games_world6c, world6 )
+Game.create_from_ary!( games_world7c, world7 )
+Game.create_from_ary!( games_world8c, world8 )
+Game.create_from_ary!( games_world9c, world9 )
+Game.create_from_ary!( games_world10c, world10 )
+Game.create_from_ary!( games_world11c, world11 )
+Game.create_from_ary!( games_world12c, world12 )
 
 
 Prop.create!( key: 'db.world.quali.2012/13.c.version', value: '1' )
